b0b06473f96ab406d5116897e76c9dfb91226512
kiwee4 <86410929+kiwee4@users.noreply.github.com>
2021-07-09 22:38:11 +0900

    카메라 무빙

---
+++ BrawlStars_portfolio/Assets/Scripts/CameraMoving/Main_Camera_Moving.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main_Camera_Moving : MonoBehaviour
+{
+    public GameObject playercamera;
+    public GameObject player;
+    public float speed = 0.0f;
+    public bool startmove = true;
+    Coroutine cameramove = null;
+           
+    void Start()
+    {
+        if (startmove == true)
+            cameramove = StartCoroutine(StartCameraMoving());
+        else
+        {
+            this.transform.position = playercamera.transform.position;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (cameramove == null)
+        {
+            
+        }
+    }
+
+    IEnumerator StartCameraMoving()
+    {
+        Vector3 TargetPos = this.transform.position;
+        Vector3 dir = playercamera.transform.position - this.transform.position;
+        float dist = dir.magnitude;
+        dir.Normalize();
+
+        while(dist > 0.0f)
+        {
+            float delta = speed * Time.deltaTime;
+            if (dist - delta < 0.0f)
+            {
+                delta = dist;
+            }
+            dist -= delta;
+            this.transform.Translate(dir * delta);
+            //TargetPos += dir * delta;
+            //this.transform.position = Vector3.Lerp(this.transform.position, TargetPos, Time.deltaTime * 1.0f);
+            //
+            //if (Vector3.Distance(this.transform.position, TargetPos) < 0.1f)
+            //{
+            //    this.transform.position = TargetPos;
+            //}
+            yield return null;
+        }
+        cameramove = null;
+    }
+}

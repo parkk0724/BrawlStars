8431e596910879a12af9a18a3c74ac6b3c58f109
KimGeonHea <86962177+KimGeonHea@users.noreply.github.com>
2021-07-18 13:52:30 +0900

    BulletCase
    벌릿케이스

--- BrawlStars_portfolio/Assets/Scripts/Character/Jester/Jester.cs
+++ BrawlStars_portfolio/Assets/Scripts/Character/Jester/Jester.cs
@@ -11,8 +11,10 @@ public class Jester : Hero
     [SerializeField] bool m_fFireReady;
     [SerializeField] float m_fFireRate;
     [SerializeField] float m_fCurfireTime;
-    [SerializeField] Transform[] m_objBulletPos;
+    [SerializeField] Transform[] m_tBulletPos;
     [SerializeField] GameObject m_objbullet;
+    [SerializeField] Transform m_tBulletPosCase;
+    [SerializeField] GameObject m_objBulletCase;
     Animator[] anim;
     UnityEngine.Coroutine m_cAttack = null;
     protected override void Start()
@@ -54,7 +56,6 @@ public class Jester : Hero
     {
         if (Input.GetMouseButtonDown(0))
         {
-            //Instantiate(m_objbullet, m_objBulletPos[0].position, m_objBulletPos[0].rotation);
             if (m_tfResultTarget == null) //Å¸°ÙÀÌ ¾øÀ»‹š
             {
                 Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -85,12 +86,6 @@ public class Jester : Hero
                     m_bRotStart = true;
                     StartCoroutine(BulletInit());
                     m_bCheckStart = false;
-                    //if (!m_fFireReady)
-                    //{
-                    //    m_bMoveStart = true;
-                    //    m_bRotStart = false;
-                    //}
-
                 }
                 yield return null;
             }
@@ -100,87 +95,71 @@ public class Jester : Hero
     {
         
         if (m_bCheckStart)
-        {
-            Vector3 Bojung = m_tfResultTarget.position - m_objBulletPos[0].position;
+        {  /*
+            Vector3 Bojung = m_tfResultTarget.position - m_tBulletPos[0].position;
             Vector3 yBojung = new Vector3(Bojung.x, 0, Bojung.z);
             yBojung.Normalize();
-            Vector3 Bojung_1 = m_tfResultTarget.position - m_objBulletPos[1].position;
+            Vector3 Bojung_1 = m_tfResultTarget.position - m_tBulletPos[1].position;
             Vector3 yBojung_1 = new Vector3(Bojung.x, 0, Bojung.z);
             yBojung.Normalize();
-            Vector3 Bojung_2 = m_tfResultTarget.position - m_objBulletPos[2].position;
+            Vector3 Bojung_2 = m_tfResultTarget.position - m_tBulletPos[2].position;
             Vector3 yBojung_2 = new Vector3(Bojung.x, 0, Bojung.z);
             yBojung.Normalize();
             yBojung_1.Normalize();
-            yBojung_2.Normalize();
+            yBojung_2.Normalize();*/
 
             yield return new WaitForSeconds(0.15f);
-            GameObject instantBullet = Instantiate(m_objbullet, m_objBulletPos[0].position, m_objBulletPos[0].rotation);
+            GameObject instantBullet = Instantiate(m_objbullet, m_tBulletPos[0].position, m_tBulletPos[0].rotation);
             Rigidbody bulletRigid = instantBullet.GetComponent<Rigidbody>();
-            bulletRigid.velocity = m_objBulletPos[0].forward * 30f;
-            //bulletRigid.velocity = yBojung * 30f;
+            bulletRigid.velocity = m_tBulletPos[0].forward * 30f;
+            BullutCaseInit(2f);
+
             yield return new WaitForSeconds(0.05f);
-            GameObject instantBullet_1 = Instantiate(m_objbullet, m_objBulletPos[1].position, m_objBulletPos[1].rotation);
+            GameObject instantBullet_1 = Instantiate(m_objbullet, m_tBulletPos[1].position, m_tBulletPos[1].rotation);
             Rigidbody bulletRigid_1 = instantBullet_1.GetComponent<Rigidbody>();
-            bulletRigid_1.velocity = m_objBulletPos[1].forward * 30f;
-            //bulletRigid_1.velocity = yBojung_1 * 30f;
+            bulletRigid_1.velocity = m_tBulletPos[1].forward * 30f;
+            BullutCaseInit(2f);
+
             yield return new WaitForSeconds(0.05f);
-            GameObject instantBullet_2 = Instantiate(m_objbullet, m_objBulletPos[2].position, m_objBulletPos[2].rotation);
+            GameObject instantBullet_2 = Instantiate(m_objbullet, m_tBulletPos[2].position, m_tBulletPos[2].rotation);
             Rigidbody bulletRigid_2 = instantBullet_2.GetComponent<Rigidbody>();
-            bulletRigid_2.velocity = m_objBulletPos[2].forward * 30f;
-            //bulletRigid_2.velocity = yBojung_2 * 30f;
+            bulletRigid_2.velocity = m_tBulletPos[2].forward * 30f;
+            BullutCaseInit(2f);
+
         }
         if (!m_bCheckStart)
         {
 
-            GameObject instantBullet = Instantiate(m_objbullet, m_objBulletPos[0].position, m_objBulletPos[0].rotation);
+            GameObject instantBullet = Instantiate(m_objbullet, m_tBulletPos[0].position, m_tBulletPos[0].rotation);
             Rigidbody bulletRigid = instantBullet.GetComponent<Rigidbody>();
-            bulletRigid.velocity = m_objBulletPos[0].forward * 30f;
+            bulletRigid.velocity = m_tBulletPos[0].forward * 30f;
+            BullutCaseInit(2f);
             yield return new WaitForSeconds(0.05f);
-            GameObject instantBullet_1 = Instantiate(m_objbullet, m_objBulletPos[1].position, m_objBulletPos[1].rotation);
+
+            GameObject instantBullet_1 = Instantiate(m_objbullet, m_tBulletPos[1].position, m_tBulletPos[1].rotation);
             Rigidbody bulletRigid_1 = instantBullet_1.GetComponent<Rigidbody>();
-            bulletRigid_1.velocity = m_objBulletPos[1].forward * 30f;
+            bulletRigid_1.velocity = m_tBulletPos[1].forward * 30f;
+            BullutCaseInit(2f);
             yield return new WaitForSeconds(0.05f);
-            GameObject instantBullet_2 = Instantiate(m_objbullet, m_objBulletPos[2].position, m_objBulletPos[2].rotation);
+
+            GameObject instantBullet_2 = Instantiate(m_objbullet, m_tBulletPos[2].position, m_tBulletPos[2].rotation);
             Rigidbody bulletRigid_2 = instantBullet_2.GetComponent<Rigidbody>();
-            bulletRigid_2.velocity = m_objBulletPos[2].forward * 30f;
+            bulletRigid_2.velocity = m_tBulletPos[2].forward * 30f;
+            BullutCaseInit(2f);
         }
 
     }
-    void BasicAttack()
+
+    void BullutCaseInit(float time)
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            if (m_tfResultTarget != null)
-            {
-                m_bMoveStart = false;
-                anim[1].SetTrigger("doShoot");
-                m_Animator.SetTrigger("tBAttack");
-                m_fCurfireTime = 0;
-
-            }
-        }
-        if (Input.GetMouseButtonDown(0))
-        {
-            if (m_tfResultTarget == null)
-            {
-                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-                RaycastHit hit;
-                if (Physics.Raycast(ray, out hit, 1000.0f, m_lmPicking_Mask))
-                {
-                    if (m_fFireReady)
-                    {
-                        this.transform.LookAt(hit.point);
-                    }
-                }
-                m_bMoveStart = false;
-                //m_bRotStart = true;
-                anim[1].SetTrigger("doShoot");
-                m_Animator.SetTrigger("tBAttack");
-                m_fCurfireTime = 0;
-
-            }
-        }
+        GameObject instanBulletCase = Instantiate(m_objBulletCase, m_tBulletPosCase.position, m_tBulletPosCase.rotation);
+        Rigidbody bulletcaseRigid = instanBulletCase.GetComponent<Rigidbody>();
+        Vector3 pos = m_tBulletPosCase.forward * Random.Range(-3, -2) + m_tBulletPosCase.up * Random.Range(2, 5);
+        bulletcaseRigid.AddForce(pos, ForceMode.Impulse);
+        bulletcaseRigid.AddTorque(Vector3.up * Random.Range(-10, 10));
+        Destroy(instanBulletCase, time);
     }
+
     public override void SkillAttack()
     {
 

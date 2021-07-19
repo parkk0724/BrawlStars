440b523e3931d026141e0487d3641d89360d85cc
kiwee4 <86410929+kiwee4@users.noreply.github.com>
2021-07-19 21:44:04 +0900

    솔저 폴리싱

--- BrawlStars_portfolio/Assets/Scripts/Animation/Animation_Event.cs
+++ BrawlStars_portfolio/Assets/Scripts/Animation/Animation_Event.cs
@@ -7,11 +7,29 @@ public class Animation_Event : MonoBehaviour
     [Header("Bazooka")]    
     public UnityAction bazooka_basic_fire = null;
     public UnityAction bazooka_skill_fire = null;
+    public GameObject bazooka_fire_effect;
+    public Transform bazooka_fire_effect_pos;
+    public GameObject run_effect;
+    public Transform Lfoot_pos;
+    public Transform Rfoot_pos;
 
     public UnityAction OnShoot = null;
     public UnityAction OnSkillShoot = null;
-           
+
     // Start is called before the first frame update
+
+    private void Run_Effect_L()
+    {
+        GameObject fire_effect = Instantiate(run_effect, Lfoot_pos.position, Quaternion.identity);
+    }
+    private void Run_Effect_R()
+    {
+        GameObject fire_effect = Instantiate(run_effect, Rfoot_pos.position, Quaternion.identity);
+    }
+    private void Bazooka_Effect()
+    {
+        GameObject fire_effect = Instantiate(bazooka_fire_effect, bazooka_fire_effect_pos.position, bazooka_fire_effect_pos.rotation);
+    }
     private void Bazooka_Basic_Fire()
     {
         bazooka_basic_fire?.Invoke();

4181bb11d57420f9e8dc74c995ba5179e201cd11
KimGeonHea <86962177+KimGeonHea@users.noreply.github.com>
2021-08-09 01:23:35 +0900

    총알레이케스트로처리
    총알레이케스트로처리

--- BrawlStars_portfolio/Assets/Scripts/Character/Jester/JesterBullet.cs
+++ BrawlStars_portfolio/Assets/Scripts/Character/Jester/JesterBullet.cs
@@ -14,6 +14,8 @@ public class JesterBullet : MonoBehaviour
     public GameObject HitEffect = null;
     float Speed = 20f;
     Vector3 Originpos;
+    public LayerMask layerMask_t = 0;
+    public LayerMask layerMask_o = 0;
     void Start()
     {
         //hero = GetComponent<Hero>();
@@ -32,6 +34,27 @@ public class JesterBullet : MonoBehaviour
         Vector3 nextPos = curPos + this.transform.forward * Speed * Time.deltaTime;
         this.transform.position = nextPos;
         float dist = Vector3.Distance(Originpos, nextPos);
+        float dist_2 = Vector3.Distance(curPos, nextPos);
+        RaycastHit hit;
+        if(Physics.Raycast(curPos ,nextPos-curPos ,out hit ,dist_2 , layerMask_t))
+        {
+            if(hit.transform.gameObject.GetComponent<Monster>())
+            {
+                GameObject player = GameObject.Find("Jester");
+                Jester jester = player.GetComponent<Jester>();
+                jester.FeverUp();
+                hit.transform.gameObject.GetComponent<Monster>().Hit(20, Color.red);
+                GameObject obj = Instantiate(HitEffect, this.transform.position, this.transform.rotation);
+                Destroy(obj.gameObject, 0.2f);
+                Destroy(this.gameObject);
+            }
+        }
+        if(Physics.Raycast(curPos ,nextPos-curPos ,out hit ,dist_2 , layerMask_o))
+        {
+            GameObject obj = Instantiate(HitEffect, this.transform.position, this.transform.rotation);
+            Destroy(obj.gameObject, 0.2f);
+            Destroy(this.gameObject);
+        }
         if(dist > 10)
         {
             Destroy(gameObject);
@@ -39,24 +62,24 @@ public class JesterBullet : MonoBehaviour
     }
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Monster"))
-        {
-
-            //Jester jester = GameObject.FindWithTag("Player").GetComponent<Jester>();
-            GameObject obj = GameObject.Find("Jester");
-            Jester jester = obj.GetComponent<Jester>();
-            jester.FeverUp();
-            if (other.GetComponent<Monster>())
-            {
-                other.GetComponent<Monster>().Hit(jester.GetATK() + Random.Range(-5, 5), Color.red);
-
-            }
-        }
-        if (other.gameObject.CompareTag("Monster") || other.gameObject.CompareTag("Wall") || other.gameObject.CompareTag("Obstacle"))
-        {
-            GameObject obj = Instantiate(HitEffect, other.transform.position, this.transform.rotation);
-            Destroy(this.gameObject);
-            Destroy(obj.gameObject, 0.2f);
-        }
+        //if (other.gameObject.CompareTag("Monster"))
+        //{
+        //
+        //    //Jester jester = GameObject.FindWithTag("Player").GetComponent<Jester>();
+        //    GameObject obj = GameObject.Find("Jester");
+        //    Jester jester = obj.GetComponent<Jester>();
+        //    jester.FeverUp();
+        //    if (other.GetComponent<Monster>())
+        //    {
+        //        other.GetComponent<Monster>().Hit(jester.GetATK() + Random.Range(-5, 5), Color.red);
+        //
+        //    }
+        //}
+        //if (other.gameObject.CompareTag("Monster") || other.gameObject.CompareTag("Wall") || other.gameObject.CompareTag("Obstacle"))
+        //{
+        //    GameObject obj = Instantiate(HitEffect, other.transform.position, this.transform.rotation);
+        //    Destroy(this.gameObject);
+        //    Destroy(obj.gameObject, 0.2f);
+        //}
     }
 }

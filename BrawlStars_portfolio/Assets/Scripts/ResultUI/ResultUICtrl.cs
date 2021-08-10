6c5572e2c8036ac9d2e7c85d3c1fcbd5613346bc
KrJunSeong <bws4115@gmail.com>
2021-08-11 02:34:27 +0900

    Result UI Work

--- BrawlStars_portfolio/Assets/Scripts/ResultUI/ResultUICtrl.cs
+++ BrawlStars_portfolio/Assets/Scripts/ResultUI/ResultUICtrl.cs
@@ -7,42 +7,50 @@ using UnityEngine.SceneManagement;
 
 public class ResultUICtrl : MonoBehaviour
 {
-    private Transform[] objs;
+    private Transform[] arrObjs = new Transform[3];
     private AudioClip StarAudio;
     private TextMeshProUGUI TimeTxt;
+    private Button BtnRetry;
+    private Button BtnMainMenu;
+    private AudioSource BackgroundMuisc;
+    private int nGrade = 0;
 
     private void Start()
     {
-        SetStars(60f);
-        SetClearTime(90f);
+        InitTrans();
+        SetStars(90f);     // 정상구동 할려면 파라미터 GameManager.instance.m_fTime 로 바꿀 것
+        SetClearTime(90f); // 정상구동 할려면 파라미터 GameManager.instance.m_fTime 로 바꿀 것
+    }
+    private void Update()
+    {
+        for(int i = 0; i < nGrade; i++)
+        {
+            arrObjs[i].transform.Rotate(Vector3.up * Time.deltaTime * 100f);
+        }
     }
 
-    void SetStars(float fTime)
-    {    
-        // 비활성 오브젝트 출력
-        objs = GetComponentsInChildren<Transform>(true);
-
+    private void InitTrans()
+    {
         // -------------------------------------- 개체 갖고 오기 --------------------------------------
-        foreach (Transform obj in objs)
+        Transform[] TempObjs = GetComponentsInChildren<Transform>(true);
+
+        foreach (Transform trs in TempObjs)
         {
-            if (obj.name == "Star1") { objs[0] = obj; }
-            else if (obj.name == "Star2") { objs[1] = obj; }
-            else if (obj.name == "Star3") { objs[2] = obj; }
-            
-            if(obj.name == "TxtClearTimeNum") 
-            {
-                TimeTxt = obj.GetComponent<TextMeshProUGUI>();
-            }
+            if (trs.name == "Star1")                { arrObjs[0] = trs; }
+            else if (trs.name == "Star2")           { arrObjs[1] = trs; }
+            else if (trs.name == "Star3")           { arrObjs[2] = trs; }
+            else if (trs.name == "TxtClearTimeNum") { TimeTxt = trs.GetComponent<TextMeshProUGUI>(); }
+            else if (trs.name == "BtnRetry")        { BtnRetry = trs.GetComponent<Button>(); }
+            else if (trs.name == "BtnMainMenu")     { BtnMainMenu = trs.GetComponent<Button>(); }
         }
 
-        
-        //foreach (TextMeshProUGUI txt in GetComponentsInChildren<TextMeshProUGUI>(true))
-        //{
-        //    if (txt.name == "TxtClearTimeNum") TimeTxt = txt;
-        //}
-
+        BtnRetry.onClick.AddListener(Retry);
+        BtnMainMenu.onClick.AddListener(LoadMainMenu);
+    }
+    void SetStars(float fTime)
+    { 
         // ---------------------------------------- 점수처리(남은 클리어 타임) ------------------------------
-        int nGrade = 0;
+        
         while (fTime >= 30.0f && nGrade < 4)
         {
             fTime -= 30.0f;                     // 30초 마다 별 한 개씩 생성.
@@ -52,9 +60,11 @@ public class ResultUICtrl : MonoBehaviour
         // -------------------------------------- 점수에 따른 별 출력 --------------------------------------
         for (int i = 0; i < nGrade; i++)         
         {
-            objs[i].gameObject.SetActive(true);
+            arrObjs[i].gameObject.SetActive(true);
+            
         }
     }
+
 
     void SetClearTime(float fTime) // 파라미터의 float 값을 남은 시간으로 출력해주는 함수
     {
@@ -69,14 +79,13 @@ public class ResultUICtrl : MonoBehaviour
     
     void Retry()
     {
-        // 
+        SceneManager.LoadScene("Browl_Stars");
     }
 
-    void Ld()
+    void LoadMainMenu()
     {
-        // 구현해야 할 것
-        // button, Onclick에 함수추가
-        SceneManager.LoadScene("CharacterSelect");
+        SceneManager.LoadScene("MainMenu");
+        
     }
 }
 

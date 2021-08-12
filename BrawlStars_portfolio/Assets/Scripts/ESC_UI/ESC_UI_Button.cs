d393df621840e160ca63263f0dc242ffc1a5d125
kiwee4 <86410929+kiwee4@users.noreply.github.com>
2021-08-12 22:48:26 +0900

    esc 화면 폴리싱

--- BrawlStars_portfolio/Assets/Scripts/ESC_UI/ESC_UI_Button.cs
+++ BrawlStars_portfolio/Assets/Scripts/ESC_UI/ESC_UI_Button.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class ESC_UI_Button : MonoBehaviour
 {
+    private GameObject ClickSound;
+
+    private void Awake()
+    {
+        ClickSound = GameObject.Find("ClickSound");
+    }
     public void OnClick_Quit()
     {
+        PlayClickSound();
 #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
 #else
@@ -15,7 +22,13 @@ public class ESC_UI_Button : MonoBehaviour
 
     public void OnClick_Continue()
     {
+        PlayClickSound();
         Time.timeScale = 1.0f;
         ESC_UI.Instance.Exit_UI();
+    }
+
+    public void PlayClickSound()
+    {
+        ClickSound.GetComponent<AudioSource>().Play();
     }
 }

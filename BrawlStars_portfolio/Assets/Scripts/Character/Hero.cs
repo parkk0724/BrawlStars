8273937333a5ce3208e796828254a62e972e434c
kiwee4 <86410929+kiwee4@users.noreply.github.com>
2021-08-12 22:19:10 +0900

    오류 수정

--- BrawlStars_portfolio/Assets/Scripts/Character/Hero.cs
+++ BrawlStars_portfolio/Assets/Scripts/Character/Hero.cs
@@ -88,15 +88,16 @@ public class Hero : Character
 
     protected virtual void Awake()
     {
+        m_Jump_StartSound = GameObject.Find("JumpStart");
         m_objTargetEffect = GameObject.Find("TargetIndicator").GetComponent<ParticleSystem>();
         m_objHp = GameObject.Find("HPPotionEffect");
-        m_objHp.SetActive(false);
+        //m_objHp.SetActive(false);
         m_objStamina = GameObject.Find("STPotionEffect");
-        m_objStamina.SetActive(false);
+        //m_objStamina.SetActive(false);
         m_objFever = GameObject.Find("FVPotionEffect");
-        m_objFever.SetActive(false);
+        //m_objFever.SetActive(false);
         m_objInvicible = GameObject.Find("IncivibleEffect");
-        m_objInvicible.SetActive(false);
+        //m_objInvicible.SetActive(false);
         GameObject revivalEffect = Instantiate(Resources.Load<GameObject>("Prefabs/Particles/RevivalEffect"), transform.parent);
         GameObject JumpEffect = Instantiate(Resources.Load<GameObject>("Prefabs/Particles/JumpEffect"), transform.parent);
         revivalEffect.SetActive(false);

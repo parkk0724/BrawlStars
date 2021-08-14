86a764586253480880585407b88e5e3bfd2547ac
KimGeonHea <86962177+KimGeonHea@users.noreply.github.com>
2021-08-15 02:49:56 +0900

    아이템 데이터 베이스 딕셔너리화
    아이템 데이터 베이스 딕셔너리화

--- BrawlStars_portfolio/Assets/Scripts/Item/itemDatabase.cs
+++ BrawlStars_portfolio/Assets/Scripts/Item/itemDatabase.cs
@@ -2,7 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class itemDatabase : MonoBehaviour {
+public class itemDatabase : MonoBehaviour
+{
 
     static public itemDatabase instance;
     private void Awake()
@@ -36,9 +37,9 @@ public class itemDatabase : MonoBehaviour {
     }
     void Update()
     {
-        monsterSearch(itemDorp_Nomal , itemDorp_Boss);
+        monsterSearch(itemDorp_Nomal, itemDorp_Boss);
     }
-    void monsterSearch(int DropCount , int DropCount_2)
+    void monsterSearch(int DropCount, int DropCount_2)
     {
         monster = GameObject.FindGameObjectsWithTag("Monster");
         if (monster.Length <= 0)
@@ -67,52 +68,81 @@ public class itemDatabase : MonoBehaviour {
         for (int z = 0; z < _itemCount; z++) // 아이템 드랍 몇번 할꺼냐
         {
             int DropRange = Random.Range(0, 10);
-            if (DropRange < 4)
+            if (DropRange < 1)
             {
                 return;
             }
             else if (DropRange < 9)
             {
                 ArrayList table = new ArrayList();
-                for (int i = 0; i < instance.ItemData.Count; i++)
+
+                foreach (var keyValue in ItemData)
                 {
-                    if (instance.ItemData[i][i].itemGrade == "D") // 등급비교
+                    for (int i = 0; i < keyValue.Value.Count; i++)
                     {
-                        table.Add(i);//테이블에 아이템의 인덱스값을 저장
+                        Debug.Log(keyValue.Value[i].itemGrade);
+                        if (keyValue.Value[i].itemGrade == "D") //키 벨루의 아이템 등급을 비교한다
+                        {
+                            table.Add(keyValue.Key);
+                        }
                     }
                 }
+                #region 실패의 방법
+                //for (int i = 0; i < instance.ItemData.Count; i++)
+                //{
+                //    for (int j = 0; j < instance.ItemData[i].Count; j++)
+                //    {
+                //        if(instance.ItemData[i][j].itemGrade == "D")
+                //        {
+                //            table.Add(i);
+                //        }
+                //    }
+                //}
+                //Prefabs/Item/FeverPotion
+                #endregion
                 int tableindex = Random.Range(0, table.Count);// 테이블의 값을 랜덤으로 돌린 인덱스값
-                GameObject obj = Resources.Load<GameObject>(instance.ItemData[(int)table[tableindex]][(int)table[tableindex]].itemPrefab);
-                obj.GetComponent<DropItem>().reitem.itemtype = instance.ItemData[(int)table[tableindex]][(int)table[tableindex]].itemtype;
-                obj.GetComponent<DropItem>().reitem.itemName = instance.ItemData[(int)table[tableindex]][(int)table[tableindex]].itemName;
-                obj.GetComponent<DropItem>().reitem.itemGrade = instance.ItemData[(int)table[tableindex]][(int)table[tableindex]].itemGrade;
-                obj.GetComponent<DropItem>().reitem.itemCount = instance.ItemData[(int)table[tableindex]][(int)table[tableindex]].itemCount;
-                obj.GetComponent<DropItem>().reitem.uSEitem = instance.ItemData[(int)table[tableindex]][(int)table[tableindex]].uSEitem;
-                Instantiate(obj, monsterTansform.position, obj.transform.rotation);
+
+                List<reitem> mydate = new List<reitem>(); //새로운 리스트를 만들어서 그곳에 값을 저장하고
+                mydate = instance.ItemData[(int)table[tableindex]];// 그것을 넣는다
+                GameObject obj = Resources.Load<GameObject>(mydate[0].itemPrefab);
+                obj.GetComponent<DropItem>().reitem.Key = mydate[0].Key;
+                obj.GetComponent<DropItem>().reitem.itemtype = mydate[0].itemtype;
+                obj.GetComponent<DropItem>().reitem.itemName = mydate[0].itemName;
+                obj.GetComponent<DropItem>().reitem.itemGrade = mydate[0].itemGrade;
+                obj.GetComponent<DropItem>().reitem.itemCount = mydate[0].itemCount;
+                obj.GetComponent<DropItem>().reitem.uSEitem = mydate[0].uSEitem;
+                Instantiate(obj, monsterTansform.position, obj.transform.rotation); //데이터를 넣는다
             }
             else if (DropRange < 10)
             {
                 ArrayList table = new ArrayList();
-                for (int i = 0; i < instance.ItemData.Count; i++)
+                foreach (var keyValue in ItemData)
                 {
-                    if (instance.ItemData[i][i].itemGrade == "C") // 등급비교
+                    for (int i = 0; i < keyValue.Value.Count; i++)
                     {
-                        table.Add(i);//테이블에 아이템의 인덱스값을 저장
+                        Debug.Log(keyValue.Value[i].itemGrade);
+                        if (keyValue.Value[i].itemGrade == "C")
+                        {
+                            table.Add(keyValue.Key);
+                        }
                     }
                 }
                 int tableindex = Random.Range(0, table.Count);
                 //생성된 드랍아이템의 아이템 타입을 아이템데이터베이스 형식으로 바꿈
-                GameObject obj = Resources.Load<GameObject>(instance.ItemData[(int)table[tableindex]][(int)table[tableindex]].itemPrefab);
-                obj.GetComponent<DropItem>().reitem.itemtype = instance.ItemData[(int)table[tableindex]][(int)table[tableindex]].itemtype;
-                obj.GetComponent<DropItem>().reitem.itemName = instance.ItemData[(int)table[tableindex]][(int)table[tableindex]].itemName;
-                obj.GetComponent<DropItem>().reitem.itemGrade = instance.ItemData[(int)table[tableindex]][(int)table[tableindex]].itemGrade;
-                obj.GetComponent<DropItem>().reitem.itemCount = instance.ItemData[(int)table[tableindex]][(int)table[tableindex]].itemCount;
-                obj.GetComponent<DropItem>().reitem.uSEitem = instance.ItemData[(int)table[tableindex]][(int)table[tableindex]].uSEitem;
+                List<reitem> mydate = new List<reitem>();
+                mydate = instance.ItemData[(int)table[tableindex]];
+                GameObject obj = Resources.Load<GameObject>(mydate[0].itemPrefab);
+                obj.GetComponent<DropItem>().reitem.Key = mydate[0].Key;
+                obj.GetComponent<DropItem>().reitem.itemtype = mydate[0].itemtype;
+                obj.GetComponent<DropItem>().reitem.itemName = mydate[0].itemName;
+                obj.GetComponent<DropItem>().reitem.itemGrade = mydate[0].itemGrade;
+                obj.GetComponent<DropItem>().reitem.itemCount = mydate[0].itemCount;
+                obj.GetComponent<DropItem>().reitem.uSEitem = mydate[0].uSEitem;
                 Instantiate(obj, monsterTansform.position, obj.transform.rotation);
             }
         }
     }
-#region FirstSolution // 처음에 만든 아이템 드랍방식
+    #region FirstSolution // 처음에 만든 아이템 드랍방식
     /*
     public void ItemDrop(Transform monsterTansform, int _itemCount) // 아이템드랍
     {
@@ -163,19 +193,21 @@ public class itemDatabase : MonoBehaviour {
             }
         } 
     }*/
-#endregion
+    #endregion
     void LoadItemTextData() //데이터화 시켜서 불러옴
     {
         TextAsset textAsset = Resources.Load<TextAsset>("TextData/Item");
 
         string temp = textAsset.text.Replace("\r\n", "\n");
         string[] row = temp.Split('\n');
-        List<reitem> myitem = new List<reitem>();
+
 
         for (int i = 1; i < row.Length; i++)
         {
             string[] data = row[i].Split(',');
 
+            if (data.Length <= 1) continue;
+            List<reitem> myitem = new List<reitem>(); //리스트가 위에서 실행하면 중복으로 실행이 되서 밑으로 빼야함
             myitem.Add(new reitem(int.Parse(data[0]), data[1], data[2], data[3], data[4], int.Parse(data[5]), data[6]));
 
             ItemData.Add(int.Parse(data[0]), myitem);
